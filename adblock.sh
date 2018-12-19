0e9ecabcf42a607aaff88c08707e366c97373c8f
ssolifd <ssolifd@student.cccs.edu>
2018-12-19 08:31:06 +0000

    add

--- adblock.sh
+++ adblock.sh
@@ -11,7 +11,7 @@ fi
 \cp -f adblock.sh /opt/adblock.sh
 
 # IP address to send traffic
-blackHoleDNS="0.0.0.0"
+blackHoleDNS="127.0.0.1"
 blackHoleDNS6="::"
 
 # Location of configuration file
@@ -30,16 +30,16 @@ echo "Please wait while we gather and install blocking lists..."
 
 curl -s http://pgl.yoyo.org/adservers/serverlist.php? | grep -P '(?=^.{5,254}$)(^(?:(?!\d+\.)[a-zA-Z0-9_\-]{1,63}\.?)+(?:[a-zA-Z]{2,})$)' >> /tmp/blackhole
 # Retrieve lists
-curl -s http://www.malwaredomainlist.com/hostslist/hosts.txt | grep -v "#" | awk '{print $2}' >> /tmp/blackhole
-curl -s http://winhelp2002.mvps.org/hosts.txt | grep -v "#" | awk '{print $2}' | less >> /tmp/blackhole
-curl -s http://someonewhocares.org/hosts/hosts | grep -v "#" | awk '{print $2}' >> /tmp/blackhole
-curl -s https://hosts-file.net/ad_servers.txt | grep -v "#" | awk '{print $2}' >> /tmp/blackhole
-curl -s https://adaway.org/hosts.txt | grep -v "#" | awk '{print $2}' >> /tmp/blackhole
-curl -s http://malwaredomains.lehigh.edu/files/justdomains >> /tmp/blackhole
-curl -s https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt | grep -v "#" >> /tmp/blackhole
-curl -s https://s3.amazonaws.com/lists.disconnect.me/simple_malvertising.txt | grep -v "#" >> /tmp/blackhole
-curl -s https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt | grep -v "#" >> /tmp/blackhole
-curl -s https://s3.amazonaws.com/lists.disconnect.me/simple_malware.txt | grep -v "#" >> /tmp/blackhole
+#curl -s http://www.malwaredomainlist.com/hostslist/hosts.txt | grep -v "#" | awk '{print $2}' >> /tmp/blackhole
+#curl -s http://winhelp2002.mvps.org/hosts.txt | grep -v "#" | awk '{print $2}' | less >> /tmp/blackhole
+#curl -s http://someonewhocares.org/hosts/hosts | grep -v "#" | awk '{print $2}' >> /tmp/blackhole
+#curl -s https://hosts-file.net/ad_servers.txt | grep -v "#" | awk '{print $2}' >> /tmp/blackhole
+#curl -s https://adaway.org/hosts.txt | grep -v "#" | awk '{print $2}' >> /tmp/blackhole
+#curl -s http://malwaredomains.lehigh.edu/files/justdomains >> /tmp/blackhole
+#curl -s https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt | grep -v "#" >> /tmp/blackhole
+#curl -s https://s3.amazonaws.com/lists.disconnect.me/simple_malvertising.txt | grep -v "#" >> /tmp/blackhole
+#curl -s https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt | grep -v "#" >> /tmp/blackhole
+#curl -s https://s3.amazonaws.com/lists.disconnect.me/simple_malware.txt | grep -v "#" >> /tmp/blackhole
 curl -s https://raw.githubusercontent.com/ssolifd/1/master/hosts | grep -v "#" >> /tmp/blackhole
 # Sanatize output to only domains in /tmp/blackholedomains
 cat /tmp/blackhole | uniq -u > /tmp/blackholeuniq
@@ -59,12 +59,37 @@ done
 # Remove carraige return and output configuration file
 tr -d '\r' < /tmp/blackholeconfig > $configurationFile
 
+rm -rf /tmp/blackholeconfig
+echo "Please wait while we gather and install blocking lists..."
+curl -s https://raw.githubusercontent.com/ssolifd/x/master/ad > /tmp/blackhole
+sed -i /#/d  /tmp/blackhole
+cat /tmp/blackhole | uniq -u > /tmp/blackholeuniq
+cat /tmp/blackholeuniq | grep -v "localhost" | grep -v "local" | grep -v "127.0.0.1" | grep -v "::1" > /tmp/blackholedomains
+sed -i 's/^ *//; s/ *$//; /^$/d; /^\s*$/d' /tmp/blackholedomains
 
+# Trim and format domains for unbound 
+
+for a in `cat /tmp/blackholedomains`; do
+                echo 'local-zone: "'$a'"  redirect' >> /tmp/blackholeconfig
+done
+for a in `cat /tmp/blackholedomains`; do
+                echo 'local-data: "'$a' 86400 IN A 127.0.0.1"  ' >> /tmp/blackholeconfig
+done
+for a in `cat /tmp/blackholedomains`; do
+sed -i /$a/d /etc/unbound/local.d/adaway.conf
+done
+
+# Remove carraige return and output configuration file
+tr -d '\r' < /tmp/blackholeconfig > /tmp/test
+
+cat /tmp/test >>/etc/unbound/local.d/adaway.conf
 # Cleanup
 rm -f /tmp/blackhole
 rm -f /tmp/blackholeuniq
 rm -f /tmp/blackholeconfig
-systemctl restart unbound.service > /dev/null 2>&1
 
 # Confirm completion
 echo "Installed blocklist and set to update daily"
+cd 
+docker-compose down
+docker-compose up -d
